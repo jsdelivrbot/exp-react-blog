22e5f24430b9cd880cfd2aca8febde20f6ef31ad
Guilherme Santos <guilherme.goncalvessantos@gmail.com>
2017-04-19 13:56:26 -0300

    getting posts from api

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,7 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+//import { bindActionCreators } from 'redux';
+import { fetchPosts } from '../actions/index';
 
-export default () => {
-  return (
-    <div>List of Blog Posts</div>
-  );
+class PostsIndex extends Component {
+  componentWillMount() {
+    this.props.fetchPosts();
+  }
+
+  render() {
+    return (
+      <div>List of Posts</div>
+    );
+  }
 }
+
+// this allow me to use fetchPosts in the props of this component
+/*function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ fetchPosts }, dispatch);
+}
+
+export default connect (null, mapDispatchToProps)(PostsIndex);*/
+
+// SHORTCUT
+// export default connect (null, { fetchPosts: fetchPosts })(PostsIndex);
+// line 26 using ES6:
+export default connect (null, { fetchPosts })(PostsIndex);
